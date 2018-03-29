d6d13816b1582c17e12269106ceb09be82cd8fb4
shitone <yly1991jx@gmail.com>
2018-03-29 17:14:40 +0800

    update config

--- Products/plib/cimissdata.py
+++ Products/plib/cimissdata.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib, json, ConfigParser, socket
+import urllib, json, ConfigParser, socket, datetime
 
+
+def get_jx_multi_h(hrs, type1h, timestr):
+    x=[]
+    y=[]
+    z=[]
+    now = datetime.datetime.strptime(timestr, "%Y%m%d%H0000")
+    f = urllib.urlopen('http://10.116.32.88/stationinfo/index.php/Api/stationInfoLast?type=json')
+    stdata = f.read()
+    st = json.loads(stdata)
+    st_map = {}
+    for stno in st:
+        value = ['NA' for i in range(hrs)]
+        st_map[stno] = {}
+        st_map[stno]["lontiude"] = float(st[stno]["lontiude"])
+        st_map[stno]["lattiude"] = float(st[stno]["lattiude"])
+        st_map[stno]["value"] = value
+    for hr in range(hrs):
+        ago = now - datetime.timedelta(hours=hr)
+        cdata = _get_cimiss_data_json(type1h, ago.strftime("%Y%m%d%H0000"))
+        for row in cdata:
+            if row["Station_Id_C"] in st_map:
+                st_map[row["Station_Id_C"]]["value"][hr] = float(row[type1h])
+    for stno in st_map:
+        temp_v = st_map[stno]["value"]
+        if 'NA' not in temp_v:
+            x.append(float(st_map[stno]["lontiude"]))
+            y.append(float(st_map[stno]["lattiude"]))
+            if 'PRE' in type1h:
+                z.append(sum(temp_v))
+            elif 'TEM' in type1h:
+                z.append(sum(temp_v)/hrs)
+    return x, y, z
 
 def get_jx_1h(type, timestr):
     x=[]
@@ -13,8 +45,6 @@ def get_jx_1h(type, timestr):
     st = json.loads(stdata)
 
     cdata = _get_cimiss_data_json(type, timestr)
-
-
     for row in cdata:
         stno = row["Station_Id_C"]
         if stno in st:
